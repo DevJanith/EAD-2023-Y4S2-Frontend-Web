3cafae30042440726f5ee3a63fb8054682c7d50f
janithgamage1.ed <janithgamage1.ed@gmail.com>
2023-10-09 22:51:13 +0530

    fix: update

--- src/data/train-publish.ts
+++ src/data/train-publish.ts
@@ -4,11 +4,11 @@ export interface TrainPublishType {
     description: string
 }
 
-// ==============================|| DATA - TRAIN_PUBLISH ||============================== //
+// ==============================|| DATA - TRAIN_PUBLISHED ||============================== //
 
 const trainPublish: readonly TrainPublishType[] = [
-    { id: 0, code: "PUBLISH", description: "PUBLISH" },
-    { id: 1, code: "UN-PUBLISH", description: "UN-PUBLISH" }
+    { id: 0, code: "PUBLISHED", description: "PUBLISHED" },
+    { id: 1, code: "UN-PUBLISHED", description: "UN-PUBLISHED" }
 ];
 
 export default trainPublish;
